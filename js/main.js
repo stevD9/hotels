741d129162d1c383759bf7cc4c787cf16aa55256
Fredericia <39918560+stevD9@users.noreply.github.com>
2018-11-28 23:02:23 +0200

    Fixed li:hover/fixed initial max price value

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function(){
   let rangeElement = document.getElementById("price-filter");
   let max = document.getElementById("max");
+  max.innerHTML = rangeElement.value;
 
   rangeElement.addEventListener("input",function(){
     max.innerHTML = rangeElement.value;
