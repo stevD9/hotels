24a382b543d5b29e2961b148edf8df319f3508d9
Fredericia <39918560+stevD9@users.noreply.github.com>
2018-11-29 22:30:36 +0200

    Fixed showing the clicked deal to the right side of the page, with jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
 window.addEventListener("load", function(){
+
+  // Set the variables for the range input
+
   let rangeElement = document.getElementById("price-filter");
   let max = document.getElementById("max");
   max.innerHTML = rangeElement.value;
 
+  // Set the max price on the label
+
   rangeElement.addEventListener("input",function(){
     max.innerHTML = rangeElement.value;
   });
+
+  // Set variable for the list with the deals object
+
+  const $deals = $("#hotels .deals li");
+
+  // CLick function to show the details of the clicked deal element on the right (aside) preview of the page
+
+    $deals.click(function(e){
+      console.log("CLICKED");
+
+      let $hotel = $(e.currentTarget).children(".hotel-site");
+
+      let $hotelPrice = $(e.currentTarget).children(".cost");
+
+      let $hotelPreview = $(e.currentTarget).closest(".deals").next(".selection").find(".hotel-site");
+
+      let $pricePreview = $(e.currentTarget).closest(".deals").next(".selection").find(".cost");
+
+      $($hotelPreview).html($($hotel).html());
+      $($pricePreview).html($($hotelPrice).html());
+
+    });
+
 });
